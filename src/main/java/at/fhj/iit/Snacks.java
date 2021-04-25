5b0a3be9eacb0029f8be45e6c1865cc5efbc2ba8
ChiaraZefferer <chiara.zefferer@edu.fh-joanneum.at>
2021-04-25 15:42:39 +0200

    added new getter methods in the Snack.java class, so i can test the methods afterwards

--- src/main/java/at/fhj/iit/Snacks.java
+++ src/main/java/at/fhj/iit/Snacks.java
@@ -1,42 +1,41 @@
 package at.fhj.iit;
-
-public abstract class Snacks extends Drink{
+public  class Snacks extends Drink{
     String texture;
     String taste;
-
-    public Snacks( String name) {
+    public Snacks( String name, String texture, String taste) {
         super.name = name;
+        this.texture = texture;
+        this.taste = taste;
     }
-
 
     /**
      * Creates a Snacks object with given name, e.g. chips or chocolate
      *
      *
      */
-
-
+    public String getName(){
+        return super.name;
+    }
 
     public String getTexture() {
-
         return texture;
     }
-
-    public void setTexture(String texture) {
-
-        this.texture = texture;
-    }
-
     public String getTaste() {
         return taste;
     }
-
-    public void setTaste(String taste) {
-
-        this.taste = taste;
-    }
-
     public void printSnacks() {
         System.out.println("The best snack to your Drink is " + name + ". It tastes " +taste + " and is " + texture + ".");
+    }
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+    @Override
+    public boolean isAlcoholic() {
+        return false;
     }
 }

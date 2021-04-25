2aa2ab3bca0ce02ef07064da2efb431959b36b7b
Lukas <lukas.linzer07@gmail.com>
2021-04-25 19:40:57 +0200

    Added Concters.java

--- src/main/java/at/fhj/iit/at/fhj/iit/Concerts.java
+++ src/main/java/at/fhj/iit/at/fhj/iit/Concerts.java
@@ -1,13 +1,15 @@
 package at.fhj.iit;
-public abstract class Concerts extends Drink{
+
+public class Concerts extends Drink{
 
     String kind;
     String band;
 
-    public Concerts( String name) {
+    public Concerts( String name, String kind, String band) {
         super.name = name;
+        this.kind = kind;
+        this.band = band;
     }
-
 
     /**
      * Creates a Concerts object with given name, e.g. Hip Hop or Jazz
@@ -15,26 +17,32 @@ public abstract class Concerts extends Drink{
      *
      */
 
-
+    public String getName(){
+        return name;
+    }
 
     public String getKind() {
         return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
     }
 
     public String getBand() {
         return band;
     }
 
-    public void setBand(String band) {
-        this.band = band;
-    }
-
     public void printConcerts() {
         System.out.println("The Drink tastes best to a " + kind + " " + name + ". The band is called " + band + ".");
+    }
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+    @Override
+    public boolean isAlcoholic() {
+        return false;
     }
 
 }
--- src/main/java/at/fhj/iit/at/fhj/iit/TestConcert.java
+++ src/main/java/at/fhj/iit/at/fhj/iit/TestConcert.java
@@ -1,2 +1,54 @@
-
- 
+package at.fhj.iit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+@DisplayName("Testing Concerts implementation")
+public class TestConcert {
+    private Concerts name, name2;
+    /**
+     * inits a Concert for EACH test
+     */
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        name= new Concerts("Denzel", "rappy", "Hip Hop");
+        name2 = new Concerts("Bob Marley", "soft", "sweet");
+    }
+    @Test
+    @DisplayName("Testing constructor")
+    public void testConstructor() {
+        assertEquals("Denzel", name.getName());
+        assertEquals("Bob Marley", name2.getName());
+    }
+    @Test
+    @DisplayName("Test name getter")
+    public void testNameGetter(){
+        assertEquals(name.getName(), "Denzel");
+    }
+    @Test
+    @DisplayName("Test kind getter")
+    public void testTextureGetter(){
+        assertEquals(name2.getKind(), "soft");
+    }
+    @Test
+    @DisplayName("Test band getter")
+    public void testTasteGetter(){
+        assertEquals(name2.getBand(), "sweet");
+    }
+    @Test
+    @DisplayName("Test volume getter")
+    public void testVolumeGetter(){
+        assertEquals(0, name.getVolume());
+    }
+    @Test
+    @DisplayName("Test AlcoholPercent getter")
+    public void testAlcoholPercentGetter(){
+        assertEquals(0, name2.getAlcoholPercent());
+    }
+    @Test
+    @DisplayName("Test isAlcoholic")
+    public void testisAlcoholic(){
+        assertEquals(false, name.isAlcoholic());
+    }
+}
